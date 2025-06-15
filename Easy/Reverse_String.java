5ed77941376bd86ca8426f8a67fbeebbb7c475d7
Rahul <rahulssi683@gmail.com>
2025-06-15 18:15:17 +0530

    Add Question Description

--- Easy/Reverse_String.java
+++ Easy/Reverse_String.java
@@ -1,3 +1,26 @@
+/*
+ * Problem: 344. Reverse String
+ * Difficulty: Easy
+ *
+ * Description:
+ * Write a function that reverses a string.
+ * The input string is given as an array of characters 's'.
+ *
+ * You must reverse the input array in-place with O(1) extra memory.
+ *
+ * Example 1:
+ * Input:  s = ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ *
+ * Example 2:
+ * Input:  s = ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ *
+ * Constraints:
+ * - 1 <= s.length <= 10^5
+ * - s[i] is a printable ASCII character.
+ */
+
 class Solution {
     public void reverseString(char[] s)
     {
